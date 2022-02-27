579dbffb300aa4d481f15dc329ce088a0c40b2a1
Kaçak <95632754+kacak1507@users.noreply.github.com>
2022-02-27 07:16:34 +0300

    Update libltdl7.sh to use correct package name

---
+++ IncludesLinux/bash/libjansson.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+PKG_MANAGER=$( command -v yum || command -v apt-get || command -v pacman)
+if [[ $PKG_MANAGER == *'pacman' ]]
+ then
+  sudo $PKG_MANAGER -S libjansson --noconfirm
+ else
+  sudo $PKG_MANAGER install libjansson4 -y
+fi
