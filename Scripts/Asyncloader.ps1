84a88bc1f99f59f82db071e4e4dc2a3f72cc9717
Kaçak <95632754+kacak1507@users.noreply.github.com>
2022-02-27 07:17:26 +0300

    Update API

---
+++ Scripts/Asyncloader.ps1
@@ -0,0 +1,136 @@
+﻿param($CurrentPwd)
+
+Set-Location $CurrentPwd
+
+if ($AsyncLoader.Debug -and -not $psISE -and $Session.LogLevel -ne "Silent") {Start-Transcript ".\Logs\AsyncLoader_$(Get-Date -Format "yyyy-MM-dd_HH-mm-ss").txt"}
+
+$ProgressPreference = "SilentlyContinue"
+
+Import-Module ".\Modules\Include.psm1"
+Import-Module ".\Modules\MiningRigRentals.psm1"
+Import-Module ".\Modules\PauseMiners.psm1"
+
+Set-OsFlags
+
+$Cycle = -1
+
+$StopWatch = [System.Diagnostics.StopWatch]::New()
+
+$AsyncLoader_Paused = $AsyncLoader.Pause
+
+$Hosts_LastCall = [hashtable]@{}
+
+while (-not $AsyncLoader.Stop) {
+
+    $IsVerbose = $Session.Config.EnableVerboseAsyncloader
+
+    $StopWatch.Restart()
+    $Cycle++
+    $AsyncLoader.Timestamp = (Get-Date).ToUniversalTime()
+
+    if ($IsVerbose) {
+        Write-ToFile -FilePath "Logs\errors_$(Get-Date -Format "yyyy-MM-dd").asyncloader.txt" -Message "Start cycle" -Append -Timestamp
+    }
+
+    if (-not ($Cycle % 3)) {
+        if ($IsVerbose) {
+            Write-ToFile -FilePath "Logs\errors_$(Get-Date -Format "yyyy-MM-dd").asyncloader.txt" -Message "Start Get-SysInfo" -Append -Timestamp
+        }
+        $Session.SysInfo = Get-SysInfo
+        if ($IsVerbose) {
+            Write-ToFile -FilePath "Logs\errors_$(Get-Date -Format "yyyy-MM-dd").asyncloader.txt" -Message "End Get-SysInfo" -Append -Timestamp
+        }
+    }
+
+    if (-not $AsyncLoader.Pause -and $AsyncLoader.Jobs.Count) {
+
+        $JobKeys = @($AsyncLoader.Jobs.Keys | Sort-Object {$AsyncLoader.Jobs.$_.Index} | Select-Object)
+
+        foreach ($JobKey in $JobKeys) {
+
+            $Job = $AsyncLoader.Jobs.$JobKey
+
+            if ($AsyncLoader.Pause -or -not $Job -or $Job.Running -or $Job.Paused) {
+                if ($IsVerbose) {
+                    Write-ToFile -FilePath "Logs\errors_$(Get-Date -Format "yyyy-MM-dd").asyncloader.txt" -Message "Skip job $JobKey with $($Job.Url) R=$($Job.Running) P=$($Job.Paused) AP=$($AsyncLoader.Pause)" -Append -Timestamp
+                }
+                continue
+            }
+
+            if ($Job.CycleTime -le 0) {$Job.CycleTime = $AsyncLoader.Interval}
+
+            $JobFailRetry = $false
+
+            $Now = (Get-Date).ToUniversalTime()
+
+            if (-not $Job.LastCacheWrite -or (($Job.LastCacheWrite -lt $Job.LastRequest) -and ($Job.LastCacheWrite -lt $Now.AddSeconds(-600-$Job.CycleTime)))) {
+                $RetryTime = if ($Job.LastFailCount -le 3) {600} elseif ($Job.LastFailCount -le 6) {1800} elseif ($Job.LastFailCount -le 10) {3600} else {10800}
+                if (-not $Job.LastFailRetry -or ($Job.LastFailRetry -le $Now.AddSeconds(-$RetryTime))) {
+                    $JobFailRetry = $true
+                }
+            } else {
+                $Job.LastFailCount = 0
+            }
+
+            if (-not $AsyncLoader.Pause -and $Job -and -not $Job.Running -and -not $Job.Paused -and ($JobFailRetry -or ($Job.LastRequest -le $Now.AddSeconds(-$Job.CycleTime)))) {
+
+                if ($JobFailRetry) {
+                    $Job.LastFailRetry = (Get-Date).ToUniversalTime()
+                    $Job.LastFailCount++
+                }
+
+                if ($IsVerbose) {
+                    Write-ToFile -FilePath "Logs\errors_$(Get-Date -Format "yyyy-MM-dd").asyncloader.txt" -Message "Start job $JobKey with $($Job.Url) using $($Job.Method)" -Append -Timestamp
+                }
+                try {
+                    if (($Job.Tag -eq "MiningRigRentals") -and $Job.endpoint) {
+                        Invoke-MiningRigRentalRequestAsync -Jobkey $Jobkey -force -quiet > $null
+                    } else {
+                        $JobDelay = 0
+                        $JobHost  = $Job.Host
+                        if ($JobHost) {
+                            if ($AsyncLoader.HostDelays.$JobHost -and $Hosts_LastCall.$JobHost) {
+                                $JobDelay = [Math]::Min([Math]::Max([Math]::Round($AsyncLoader.HostDelays.$JobHost - ((Get-Date).ToUniversalTime() - $Hosts_LastCall.$JobHost).TotalMilliseconds,0),0),5000)
+                                if ($JobDelay -and $IsVerbose) {
+                                    Write-ToFile -FilePath "Logs\errors_$(Get-Date -Format "yyyy-MM-dd").asyncloader.txt" -Message "Delay for $($JobDelay) milliseconds" -Append -Timestamp
+                                }
+                            }
+                        }
+
+                        Invoke-GetUrlAsync -Jobkey $Jobkey -delay $JobDelay -force -quiet > $null
+
+                        if ($JobHost) {
+                            $Hosts_LastCall.$JobHost = $Job.LastRequest
+                        }
+                    }
+                    if ($AsyncLoader.Jobs.$Jobkey.Error) {Write-ToFile -FilePath "Logs\errors_$(Get-Date -Format "yyyy-MM-dd").asyncloader.txt" -Message "Error job $JobKey with $($Job.Url) using $($Job.Method): $($AsyncLoader.Jobs.$Jobkey.Error)" -Append -Timestamp}
+                }
+                catch {
+                    if ($Error.Count){$Error.RemoveAt(0)}
+                    Write-ToFile -FilePath "Logs\errors_$(Get-Date -Format "yyyy-MM-dd").asyncloader.txt" -Message "Catch error job $JobKey with $($Job.Url) using $($Job.Method): $($_.Exception.Message)" -Append -Timestamp
+                }
+                finally {
+                    if ($IsVerbose) {
+                        Write-ToFile -FilePath "Logs\errors_$(Get-Date -Format "yyyy-MM-dd").asyncloader.txt" -Message "Done job $JobKey with $($Job.Url) using $($Job.Method)" -Append -Timestamp
+                    }
+                }
+            }
+        }
+    }
+    if ($Error.Count)  {if ($Session.LogLevel -ne "Silent") {$Error | Foreach-Object {Write-ToFile -FilePath "Logs\errors_$(Get-Date -Format "yyyy-MM-dd").asyncloader.txt" -Message "$($_.Exception.Message)" -Append -Timestamp}};$Error.Clear()}
+
+    $Delta = [Math]::Min([Math]::Max($AsyncLoader.CycleTime-$StopWatch.Elapsed.TotalSeconds,1),30)
+
+    if ($IsVerbose) {
+        Write-ToFile -FilePath "Logs\errors_$(Get-Date -Format "yyyy-MM-dd").asyncloader.txt" -Message "End cycle $(if ($Delta -gt 0) {"(wait $Delta s)"})" -Append -Timestamp
+    }
+
+    if ($AsyncLoader.Pause -ne $AsyncLoader_Paused) {
+        $AsyncLoader_Paused = $AsyncLoader.Pause
+        Write-ToFile -FilePath "Logs\errors_$(Get-Date -Format "yyyy-MM-dd").asyncloader.txt" -Message "$(if ($AsyncLoader_Paused) {"Stopping asyncloader due to"} else {"Restarting asyncloader after"}) pause" -Append -Timestamp
+    }
+
+    if ($Delta -gt 0)  {Start-Sleep -Milliseconds ($Delta*1000)}
+}
+
+if ($AsyncLoader.Debug) {Stop-Transcript}
---
+++ Scripts/StartInBackground.ps1
@@ -0,0 +1,29 @@
+﻿param($ControllerProcessID, $WorkingDirectory, $FilePath, $ArgumentList, $LogPath, $EnvVars, $Priority, $CurrentPwd)
+
+$EnvParams = @($EnvVars | Where-Object {$_ -match "^(\S*?)\s*=\s*(.*)$"} | Foreach-Object {"`$env:$($matches[1])=`"$($matches[2].Replace('"','``"'))`";"}) -join " "
+
+$ControllerProcess = Get-Process -Id $ControllerProcessID -ErrorAction Ignore
+if ($ControllerProcess -eq $null) {return}
+
+$ControllerProcess.Handle >$null
+
+$PriorityClass = @{-2 = "Idle"; -1 = "BelowNormal"; 0 = "Normal"; 1 = "AboveNormal"; 2 = "High"; 3 = "RealTime"}[$Priority]
+
+$MiningProcess = [PowerShell]::Create().AddScript("Set-Location `"$($WorkingDirectory)`"; $EnvParams (Get-Process -Id `$PID).PriorityClass = `"$($PriorityClass)`"; & `"$($FilePath)`" $($ArgumentList.Replace('"','``"')) *>&1 | Write-Verbose -Verbose")
+$MiningStatus  = $MiningProcess.BeginInvoke()
+
+do {
+    if ($ControllerProcess.WaitForExit(1000)) {
+        $MiningProcess.Streams.ClearStreams() > $null
+        $MiningProcess.Stop() > $null
+    } else {
+        if ($LogPath) {
+            $MiningProcess.Streams.Verbose.ReadAll() | Tee-Object $LogPath -Append
+        } else {
+            $MiningProcess.Streams.Verbose.ReadAll()
+        }
+        $MiningProcess.Streams.ClearStreams() > $null
+    }
+} until ($MiningStatus.IsCompleted)
+
+$MiningProcess.Dispose()
