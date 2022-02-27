6ee1612aaf47e499c66dd37948591345c80e98c8
Kaçak <95632754+kacak1507@users.noreply.github.com>
2022-02-27 07:17:06 +0300

    Update core

---
+++ Modules/Asyncloader.psm1
@@ -0,0 +1,81 @@
+﻿function Start-AsyncLoader {
+[cmdletbinding()]
+Param(
+    [Parameter(Mandatory = $False)]
+    [int]$Interval = 60,
+    [Parameter(Mandatory = $False)]
+    [bool]$Quickstart = $false
+)
+    if ($Interval -lt 60) {return}
+
+    if (-not (Test-Path ".\Cache")) {New-Item "Cache" -ItemType "directory" > $null}
+
+    $Global:AsyncLoader = [hashtable]::Synchronized(@{})
+
+    $AsyncLoader.Stop       = $false
+    $AsyncLoader.Pause      = $true
+    $AsyncLoader.Jobs       = [hashtable]@{}
+    $AsyncLoader.HostDelays = [hashtable]@{}
+    $AsyncLoader.HostTags   = [hashtable]@{}
+    $AsyncLoader.CycleTime  = 10
+    $AsyncLoader.Interval   = $Interval
+    $AsyncLoader.Quickstart = $Quickstart
+    $AsyncLoader.Debug      = $Session.LogLevel -eq "Debug"
+    $AsyncLoader.Timestamp  = $null
+
+    # Setup additional, global variables for server handling
+    $Global:AsyncLoaderListeners   = [System.Collections.ArrayList]@()
+
+     # Setup runspace to launch the AsyncLoader in a separate thread
+    $newRunspace = [runspacefactory]::CreateRunspace()
+    $newRunspace.Open()
+    $newRunspace.SessionStateProxy.SetVariable("AsyncLoader", $AsyncLoader)
+    $newRunspace.SessionStateProxy.SetVariable("Session", $Session)
+    if (Initialize-HttpClient) {
+        $newRunspace.SessionStateProxy.SetVariable("GlobalHttpClient", $Global:GlobalHttpClient)
+    }
+    $newRunspace.SessionStateProxy.Path.SetLocation($(pwd)) > $null
+
+    $AsyncloaderScript = [ScriptBlock]::Create((Get-Content ".\Scripts\Asyncloader.ps1" -Raw))
+
+    $newPS = [PowerShell]::Create().AddScript($AsyncloaderScript).AddParameters(@{'CurrentPwd'=$PWD})
+    $newPS.Runspace = $newRunspace
+
+    $Global:AsyncLoaderListeners.Add([PSCustomObject]@{
+        Runspace   = $newPS.BeginInvoke()
+		PowerShell = $newPS 
+    }) > $null
+}
+
+function Stop-AsyncLoader {
+    if (-not (Test-Path Variable:Global:Asyncloader)) {return}
+    $Global:AsyncLoader.Stop = $true
+
+    if ($Global:AsyncLoaderListeners) {
+        foreach ($Listener in $Global:AsyncLoaderListeners.ToArray()) {
+			$Listener.PowerShell.Dispose()
+			$Global:AsyncLoaderListeners.Remove($Listener)
+		}
+    }
+    $Global:AsyncLoaderListeners.Clear()
+
+    Remove-Variable "AsyncLoader" -Scope Global -Force
+}
+
+function Stop-AsyncJob {
+[cmdletbinding()]   
+Param(
+   [Parameter(Mandatory = $True)]   
+        [string]$tag
+)
+    if (-not (Test-Path Variable:Global:Asyncloader)) {return}
+    foreach ($Jobkey in @($AsyncLoader.Jobs.Keys | Select-Object)) {
+        $JobHost = $AsyncLoader.Jobs.$Jobkey.Host
+        if ($JobHost -and $AsyncLoader.HostTags.$JobHost -and ($AsyncLoader.HostTags.$JobHost -contains $tag)) {
+            $AsyncLoader.HostTags.$JobHost = @($AsyncLoader.HostTags.$JobHost | Where-Object {$_ -ne $tag})
+        }
+        if (-not $JobHost -or ($AsyncLoader.HostTags.$JobHost | Measure-Object).Count -eq 0) {
+            $AsyncLoader.Jobs.$Jobkey.Paused = $true
+        }
+    }
+}
---
+++ Modules/PingStratum.psm1
@@ -0,0 +1,78 @@
+﻿function Invoke-TcpRequest {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory = $true)]
+        [String]$Server = "localhost", 
+        [Parameter(Mandatory = $true)]
+        [String]$Port, 
+        [Parameter(Mandatory = $false)]
+        [String]$Request = "",
+        [Parameter(Mandatory = $false)]
+        [Int]$Timeout = 10, #seconds,
+        [Parameter(Mandatory = $false)]
+        [Switch]$WriteOnly
+    )
+    $Response = $null
+    try {
+        $Client = [System.Net.Sockets.TcpClient]::new()
+        #$Client.LingerState = [System.Net.Sockets.LingerOption]::new($true, 0)
+        $Client.Connect($Server, $Port)
+        $Stream = $Client.GetStream()
+        $Writer = [System.IO.StreamWriter]::new($Stream)
+        if (-not $WriteOnly) {$Reader = [System.IO.StreamReader]::new($Stream)}
+        $client.SendTimeout = $Timeout * 1000
+        $client.ReceiveTimeout = $Timeout * 1000
+        $Writer.AutoFlush = $true
+        if ($Request) {$Writer.WriteLine($Request)}
+        if (-not $WriteOnly -and $Stream.DataAvailable) {$Response = $Reader.ReadLine()}
+    }
+    catch {
+        if ($Error.Count){$Error.RemoveAt(0)}
+    }
+    finally {
+        if ($Client) {$Client.Close();$Client.Dispose()}
+        if ($Reader) {$Reader.Dispose()}
+        if ($Writer) {$Writer.Dispose()}
+        if ($Stream) {$Stream.Dispose()}
+    }
+
+    $Response
+}
+
+function Invoke-PingStratum {
+[cmdletbinding()]
+param(
+    [Parameter(Mandatory = $True)]
+    [String]$Server,
+    [Parameter(Mandatory = $True)]
+    [Int]$Port,
+    [Parameter(Mandatory = $False)]
+    [String]$User="",
+    [Parameter(Mandatory = $False)]
+    [String]$Pass="x",
+    [Parameter(Mandatory = $False)]
+    [String]$Worker=$Session.Config.WorkerName,
+    [Parameter(Mandatory = $False)]
+    [int]$Timeout = 3,
+    [Parameter(Mandatory = $False)]
+    [bool]$WaitForResponse = $False,
+    [Parameter(Mandatory = $False)]
+    [ValidateSet("Stratum","EthProxy")]
+    [string]$Method = "Stratum"
+)    
+    $Request = if ($Method -eq "EthProxy") {"{`"id`": 1, `"method`": `"login`", `"params`": {`"login`": `"$($User)`", `"pass`": `"$($Pass)`", `"rigid`": `"$($Worker)`", `"agent`": `"RainbowMiner/$($Session.Version)`"}}"} else {"{`"id`": 1, `"method`": `"mining.subscribe`", `"params`": [`"$($User)`"]}"}
+    #"{`"id`":1, `"jsonrpc`":`"2.0`", `"method`":`"eth_submitLogin`", `"params`":[`"$($User)`"]}"
+
+    try {
+        if ($WaitForResponse) {
+            $Result = Invoke-TcpRequest -Server $Server -Port $Port -Request $Request -Timeout $Timeout
+            if ($Result) {
+                $Result = ConvertFrom-Json $Result -ErrorAction Stop
+                if ($Result.id -eq 1 -and -not $Result.error) {$true}
+            }
+        } else {
+            Invoke-TcpRequest -Server $Server -Port $Port -Request $Request -Timeout $Timeout -WriteOnly > $null
+            $true
+        }
+    } catch {if ($Error.Count){$Error.RemoveAt(0)}}
+}
